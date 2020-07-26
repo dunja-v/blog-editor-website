b4cd6374cf8ad53596857d282b793ddfedd10d3b
Dunja Vesinger <dunja.vesinger@gmail.com>
2020-07-26 09:55:05 +0200

    adding author data and api

--- blog-editor/src/api/articleApi.js
+++ blog-editor/src/api/articleApi.js
@@ -1,13 +1,28 @@
 import { handleResponse } from "./apiUtils";
 
-const baseUrl = "http://localhost:3001/articles"
+const baseUrl = "http://localhost:3001"
 
 export function getArticles(){
-    return fetch(baseUrl)
+    return fetch(baseUrl + "/articles")
         .then(handleResponse);
 }
 
 export function getArticle(articleId){
-    return fetch(baseUrl  + "/" + articleId)
+    return fetch(baseUrl  + "/articles/" + articleId)
+        .then(handleResponse);
+}
+
+export function getAuthors(){
+    return fetch(baseUrl + "/authors")
+        .then(handleResponse);
+}
+
+export function getAuthor(authorId){
+    return fetch(baseUrl + "/authors/" + authorId)
+        .then(handleResponse);
+}
+
+export function getArticlesByAuthor(authorId){
+    return fetch(baseUrl + "/articles?author=" + authorId)
         .then(handleResponse);
 }
