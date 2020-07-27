317af5182f2deaa0846a1b66888389f91d46feb5
Dunja Vesinger <dunja.vesinger@gmail.com>
2020-07-27 21:55:57 +0200

    loading author's articles

--- blog-editor/src/components/author/AuthorPage.js
+++ blog-editor/src/components/author/AuthorPage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { getAuthor } from '../../api/articleApi';
+import { getAuthor, getArticlesByAuthor } from '../../api/articleApi';
 import { useParams } from "react-router-dom";
 import { Row, Col } from 'react-bootstrap'
 import './author-page.css';
 
 export function AuthorPage() {
     const [author, setAuthor] = useState({});
+    const [authoredArticles, setAuthoredArticles] = useState([]);
+
+
 
     let { authorId } = useParams();
     authorId = parseInt(authorId);
@@ -16,14 +19,23 @@ export function AuthorPage() {
             setAuthor(loadedAuthor);
         });    
     }, [authorId]);
+
+    useEffect(() => {
+        getArticlesByAuthor(authorId).then((loadedArticles) => {
+            setAuthoredArticles(loadedArticles);
+        });    
+    }, [authorId]);
   
     return (
         <Row>
         <Col></Col>
         <Col xs={10} sm={10} md={8} lg={8}>
             <Row><h2>{author.name}</h2></Row>
-            <Row>This is user summary</Row>
+            <Row>{author.summary}</Row>
             <Row><h5>Articles</h5></Row>
+            {authoredArticles.map((article) => 
+                (<Row><h5>{article.title}</h5></Row>)
+            )}
         </Col>
         <Col></Col>
         </Row>
