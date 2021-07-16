b5ee5ac8595745d72667fdd9d51d565fe66ef90d
Dunja Vesinger <dunja.vesinger@gmail.com>
2021-07-16 19:49:11 +0200

    setting up localization

--- blog-editor/src/components/page-header/PageHeader.tsx
+++ blog-editor/src/components/page-header/PageHeader.tsx
@@ -4,8 +4,11 @@ import '../../common-styles.css';
 import { Link } from "react-router-dom";
 import { Row, Col, Navbar, Nav, Container } from 'react-bootstrap';
 import { UserDropdown } from '../user-dropdown/UserDropdown';
+import { useTranslation } from "react-i18next";
 
 export function PageHeader() {
+    const {t} = useTranslation();
+
     return <Container>
         <Row className="pageHeader">
                 <Col>
@@ -19,7 +22,7 @@ export function PageHeader() {
             <Navbar >
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/">{t('header.title')}</Nav.Link>
                         <Nav.Link href="https://github.com/dunja-v/blog-editor-website">GitHub</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
--- blog-editor/src/index.tsx
+++ blog-editor/src/index.tsx
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './i18n';
+
 ReactDOM.render(
   <Router>
     <React.StrictMode>
