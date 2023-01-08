1b0e52fae126f6a9be13c32823e7ff8a1e9a9702
Dunja Vesinger <dunja.vesinger@gmail.com>
2023-01-08 11:55:49 +0100

    Remove bootstrap from HighlightArticle (#12)

--- blog-editor/src/components/highlight-article/HighlightArticle.tsx
+++ blog-editor/src/components/highlight-article/HighlightArticle.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import './highlight-article.css'
 import '../../common-styles.css';
-import { Image } from 'react-bootstrap'
 import { Link } from "react-router-dom";
 import { ArticleModel } from '../../data/models';
 import { useTranslation } from 'react-i18next';
@@ -15,16 +13,16 @@ export function HighlightArticle(props: HighlightArticleProps) {
     const date = new Date(article.date);
     const {t} = useTranslation();
 
-    return <div className="highlightArticle">
-        <Image src={article.image} fluid/>
-        <h3 className="highlightArticleTitle">
+    return <article className="highlightArticle">
+        <img src={article.image} alt="" className="highlightArticle-image"/>
+        <h2 className="highlightArticle-title">
             <Link to={"/article/" + article.id} className="textLink">{article.title}</Link>
-        </h3>        
-        <div className="highlightArticleSummary">{article.summary}</div>
-        {/**TODO replace t with Trans component and lolalize the date */}
-        <div className="highlightArticleDate">{t('highlight.article.published') + date.toLocaleDateString()}</div>
-        <div className="highlightArticleAauthor">
+        </h2>        
+        <p>{article.summary}</p>
+        {/**TODO replace t with Trans component and localize the date */}
+        <div>{t('highlight.article.published') + date.toLocaleDateString()}</div>
+        <div>
             <Link to={"/author/" + article.author} className="textLink">{article.authorName}</Link>
         </div>
-    </div>
+    </article>
 }
