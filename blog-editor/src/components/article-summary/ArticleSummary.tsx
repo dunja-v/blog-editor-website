37aa8aa02886081f1ce1a54f6c0ac1c724d766be
Dunja Vesinger <dunja.vesinger@gmail.com>
2023-01-08 10:55:11 +0100

    Redesign ArticleSummary component

--- blog-editor/src/components/article-summary/ArticleSummary.tsx
+++ blog-editor/src/components/article-summary/ArticleSummary.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
 import { ArticleModel } from '../../data/models';
+import "./article-summary.css";
 
 type ArticleSummaryProps = {
     article: ArticleModel,
@@ -10,12 +9,12 @@ export function ArticleSummary(props: ArticleSummaryProps) {
     const article = props.article;
 
     return (
-        <Card>
-            <Card.Img variant="top" src={article.image} />
-            <Card.Body>
-                <Card.Title>{article.title}</Card.Title>
-                <Card.Text>{article.summary}</Card.Text>
-            </Card.Body>
-        </Card>
+        <article className="articleSummary-container">
+            <div>
+                <h4>{article.title}</h4>
+                <p>{article.summary}</p>
+            </div>
+            <img src={article.image} className="articleSummary-image" alt="" />
+        </article>
     )    
 };
